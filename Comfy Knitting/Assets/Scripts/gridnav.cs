c12e3280a9c399848c6fb03402dff57ac4b81ab0
exiaohuaz <64718742+exiaohuaz@users.noreply.github.com>
2020-10-30 20:21:40 -0400

    Organized and started grid

---
+++ Comfy Knitting/Assets/Scripts/gridnav.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridnav : MonoBehaviour
+{  
+  [SerializeField]
+  private int rows = 10;
+  [SerializeField]
+  private int cols = 10;
+  [SerializeField]
+  private float tileSize = 1; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void generateGrid(){
+      GameObject reftile = (GameObject)Instantiate(Resources.Load("Square"));
+      for (int i = 0; i<rows; i++){
+        for (int j = 0; j<cols; j++){
+          GameObject tile = (GameObject)Instantiate(reftile, transform);
+
+          float posX = j*tileSize;
+          float posY = i*(-tileSize);
+
+          tile.transform.position = new Vector2(posX, posY);
+
+        }
+      }
+      Destroy(reftile);
+      float gridW = cols*tileSize;
+      float gridH = rows*tileSize;
+      transform.position = new Vector2(-gridW/2 + tileSize/2, gridH/2 - tileSize / 2 );
+    }
+}
