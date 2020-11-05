f8bf0adff259286e2ae275832a84f6afff6d10d2
heleaf <hfyang544@gmail.com>
2020-11-04 21:24:58 -0500

    added ui base but button functionality isn't working

---
+++ Comfy Knitting/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+        public void goToTitlePage()
+        {
+            SceneManager.LoadScene("title page");
+        }
+
+        public void goToLevelSelector()
+        {
+            SceneManager.LoadScene("level selector");
+        }
+    
+}
+
