0906e56576579bef08f2012e5c265c6cf0de1fc5
heleaf <hfyang544@gmail.com>
2020-11-04 20:46:10 -0500

    added title scrn + level selec + scene transition

---
+++ Comfy Knitting/Assets/Scripts/sceneTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneTransition : MonoBehaviour
+{
+    public void goToLevelSelector()
+    {
+        SceneManager.LoadScene("level selector");
+    }
+
+    public void goToTitlePage()
+    {
+        SceneManager.LoadScene("title page");
+    }
+}
