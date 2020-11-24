09c52c7f81cec00ebf9a183c374ab7bbded4c38a
exiaohuaz <64718742+exiaohuaz@users.noreply.github.com>
2020-11-23 22:41:31 -0500

    pause menu, some level select layout
    pause menu is fully functional -- may need to make some design choices
    
    vaguely implemented agreed upon design of the level selector

--- Comfy Knitting/Assets/Scripts/Escape.cs
+++ Comfy Knitting/Assets/Scripts/Escape.cs
@@ -1,30 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Escape : MonoBehaviour
 {
+    public GameObject pausemenu;
+
+    private bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown("escape")){
-          openPause();
+          if(!isPaused){
+            pausemenu.SetActive(true);
+            isPaused = true;
+          }
+          else{
+            pausemenu.SetActive(false);
+            isPaused = false;
+          }
         }
     }
 
-    void ExitButton(){
+    public void ExitButton(){
       Application.Quit();
     }
-    void openPause(){
-
+    public void Retry(){
+      Scene scene = SceneManager.GetActiveScene(); 
+      SceneManager.LoadScene(scene.name);
     }
-    void Retry(){
-      
+
+    public void toHome(){
+      SceneManager.LoadScene("title page");
+    }
+
+    public void toSelect(){
+      SceneManager.LoadScene("level selector");
+    }
+
+    public void Continue(){
+      pausemenu.SetActive(false);
+      isPaused = false;
     }
 }
