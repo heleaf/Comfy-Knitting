87a478b5089cc0559aab561e2fc75c04d43d7ce5
heleaf <hfyang544@gmail.com>
2020-12-02 18:54:13 -0500

    credits button functionality

---
+++ Comfy Knitting/Assets/Scripts/OpenCredits.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCredits : MonoBehaviour
+{
+    public GameObject Panel;
+    public bool isOpen = false;
+    public void openPanel() { 
+        if(Panel!=null){
+            if (isOpen == true)
+            {
+                isOpen = false;
+                Panel.SetActive(isOpen);
+            }
+            else
+            {
+                isOpen = true;
+                Panel.SetActive(isOpen);
+            }
+            
+        }
+    }
+}
